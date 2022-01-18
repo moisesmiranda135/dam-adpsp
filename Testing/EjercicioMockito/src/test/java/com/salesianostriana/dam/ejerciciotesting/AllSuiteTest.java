a6bf1d6dfe916a642a3eb957c67ba93fbad912dd
Moisés Miranda Corrales <moisesmiranda135@gmail.com>
2022-01-18 14:22:41 +0100

    se crea el proyecto y se añaden dependencias

---
+++ Testing/EjercicioMockito/src/test/java/com/salesianostriana/dam/ejerciciotesting/AllSuiteTest.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.ejerciciotesting;
+
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.*;
+import org.junit.runner.RunWith;
+
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.salesianostriana.dam.ejerciciotesting")
+public class AllSuiteTest {
+}
