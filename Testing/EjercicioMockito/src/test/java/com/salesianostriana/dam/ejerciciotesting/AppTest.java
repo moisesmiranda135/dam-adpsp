5d2cf21303f661e48643a16efd67be11e7e7bae3
Moises Miranda Corrales <moisesmiranda135@gmail.com>
2022-01-19 01:22:58 +0100

    se solucionan errores en el testing

--- Testing/EjercicioMockito/src/test/java/com/salesianostriana/dam/ejerciciotesting/AppTest.java
+++ Testing/EjercicioMockito/src/test/java/com/salesianostriana/dam/ejerciciotesting/AppTest.java
@@ -8,7 +8,7 @@ import com.salesianostriana.dam.ejerciciotesting.model.Venta;
 import com.salesianostriana.dam.ejerciciotesting.repos.ProductoRepositorio;
 import com.salesianostriana.dam.ejerciciotesting.repos.VentaRepositorio;
 import com.salesianostriana.dam.ejerciciotesting.services.VentaServicio;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -18,8 +18,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.lenient;
 
 
@@ -37,20 +38,52 @@ public class AppTest {
 
     @Test
     public void whenNewSale_thenReturnSale(){
+
+        //Creamos un cliente y le seteamos un nombre para que no este vacío
         Cliente cliente = new Cliente();
         cliente.setNombre("Moisés");
-       //productoRepositorio.save(new Producto("5", "Monitor 144Hz", 200.0));
-        LineaDeVenta lineaDeVenta = new LineaDeVenta(new Producto("5", "Monitor 144Hz", 200.0),2,350);
+
+        // He intentado haciendolo añadiendo directamente el nuevo producto al repositorio pero no se porque no funciona no lo acabo de entender
+        //productoRepositorio.save(new Producto("5", "Monitor 144Hz", 200.0));
+
+        //Creamos una linea de centa y le añadimos un producto para realizar el test
+        LineaDeVenta lineaDeVenta = new LineaDeVenta(new Producto("1","Ordenador Portátil",699.0),10,6990.0);
         List<LineaDeVenta> lista = new ArrayList<>();
         lista.add(lineaDeVenta);
         Venta v2 = new Venta(1L, LocalDate.now(),lista,cliente);
 
-        lenient().when(productoRepositorio.findOne("5")).thenReturn(new Producto("5", "Monitor 144Hz", 200.0));
+        //Compruebo que el producto encontrado en el repsoitorio coincida con el creado
+        lenient().when(productoRepositorio.findOne("1")).thenReturn(new Producto("1", "Ordenador portátil", 699.0));
 
+        //Compruebo también que cuando lo guarde en el repositorio me devuelva ese objeto
         lenient().when(ventaRepositorio.save(v2)).thenReturn(v2);
 
-        Venta v = ventaServicio.nuevaVenta(Map.of("5", 2), cliente);
+        Venta v = ventaServicio.nuevaVenta(Map.of("1", 10), cliente);
+
+        //Realizo assert para comprobar que ambos son iguales
         assertEquals(v2.getId(),v.getId());
     }
+
+    @Test
+    public void whenAddProductoToVenta_thenReturnVenta(){
+        Cliente cliente = new Cliente();
+        cliente.setNombre("Alejandro");
+        LineaDeVenta lineaDeVenta = new LineaDeVenta(new Producto("1","Ordenador Portátil",699.0),10,6990.0);
+        LineaDeVenta lineaDeVenta2 = new LineaDeVenta(new Producto("2","Ordenador de sobremesa",499.0),10,4990.0);
+        List<LineaDeVenta> lista = new ArrayList<>();
+        lista.add(lineaDeVenta);
+        Optional<Venta> optionalVenta= Optional.of(new Venta(1L, LocalDate.of(2022, 01, 18), lista, cliente));
+        lista.add(lineaDeVenta2);
+        Venta venta =new Venta(1L, LocalDate.of(2022, 01, 18), lista, cliente);
+
+        lenient().when(productoRepositorio.findOne("2")).thenReturn(new Producto("2", "Ordenador portátil", 699.0));
+        lenient().when(ventaRepositorio.findOneOptional(1L)).thenReturn(optionalVenta);
+        lenient().when(ventaRepositorio.edit(optionalVenta.get())).thenReturn(venta);
+
+        Venta v2 = ventaServicio.addProductoToVenta(1L,"2",10);
+        assertEquals(venta,v2);
+    }
+
+
 
 }
